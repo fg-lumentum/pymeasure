30249e4439378a393b247d31e84e7f920c0753b9
fredgillard <fred.gillard@lumentum.com>
2026-05-05 11:29:43 +0100

    Docs

--- pymeasure/instruments/thorlabs/__init__.py
+++ pymeasure/instruments/thorlabs/__init__.py
@@ -23,5 +23,9 @@
 #
 
 from .thorlabspm100usb import ThorlabsPM100USB
+from .thorlabspm100a import ThorlabsPM100A
+from .thorlabspm100d import ThorlabsPM100D
+from .thorlabspm100d2 import ThorlabsPM100D2
+from .thorlabspm100d3 import ThorlabsPM100D3
 from .thorlabspro8000 import ThorlabsPro8000
 from .thorlabsmbxseries import ThorlabsMBXSeries

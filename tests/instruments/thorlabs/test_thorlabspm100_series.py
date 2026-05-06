b69e41fe658ea1d0c0f3f47c6be4ab040fbdd1f0
fredgillard <fred.gillard@lumentum.com>
2026-05-06 17:05:14 +0100

    Addressing CodeRabbit nitpick, refactored tests to include all PM100 series instruments

--- tests/instruments/thorlabs/test_thorlabspm100_series.py
+++ tests/instruments/thorlabs/test_thorlabspm100_series.py
@@ -25,19 +25,30 @@
 import pytest
 
 from pymeasure.test import expected_protocol
-from pymeasure.instruments.thorlabs.thorlabspm100d import ThorlabsPM100D, SensorTypes
-from pymeasure.instruments.thorlabs.thorlabspm100d2 import ThorlabsPM100D2
+from pymeasure.instruments.thorlabs import (
+    ThorlabsPM100USB,
+    ThorlabsPM100A,
+    ThorlabsPM100D,
+    ThorlabsPM100D2,
+    ThorlabsPM100D3,
+)
+from pymeasure.instruments.thorlabs.thorlabspm100usb import SensorTypes
 
 PM100D_S130C_INIT_COMMS = ("SYST:SENSOR:IDN?", "S130C,210921324,21-SEP-2021,1,3,33")
 PM100D2_S130C_INIT_COMMS = ("SYST:SENSOR:IDN?", '"S130C","210921324","21-SEP-2021",1,3,2147484069')
 
+INSTR_CLS_PROTOCOL_LIST = [
+    (ThorlabsPM100USB, [PM100D_S130C_INIT_COMMS]),
+    (ThorlabsPM100A, [PM100D_S130C_INIT_COMMS]),
+    (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
+    (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
+    (ThorlabsPM100D3, [PM100D2_S130C_INIT_COMMS]),
+]
+
 
 def test_configure_sensor():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
-        with expected_protocol(instr_cls, protocol) as inst:
+    for instr_cls, init_protocol in INSTR_CLS_PROTOCOL_LIST:
+        with expected_protocol(instr_cls, init_protocol) as inst:
             assert inst.sensor_name == "S130C"
             assert inst.sensor_sn == "210921324"
             assert inst.sensor_cal_msg == "21-SEP-2021"
@@ -52,11 +63,8 @@ def test_configure_sensor():
 
 
 def test_wavelength_setter():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
-        protocol += [
+    for instr_cls, init_protocol in INSTR_CLS_PROTOCOL_LIST:
+        protocol = init_protocol + [
             ("SENS:CORR:WAV? MIN", 500),
             ("SENS:CORR:WAV? MAX", 2000),
             ("SENSE:CORR:WAV 1500", None),
@@ -66,104 +74,74 @@ def test_wavelength_setter():
 
 
 def test_wavelength_getter():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
-        protocol += [("SENSE:CORR:WAV?", 1500)]
+    for instr_cls, init_protocol in INSTR_CLS_PROTOCOL_LIST:
+        protocol = init_protocol + [("SENSE:CORR:WAV?", 1500)]
         with expected_protocol(instr_cls, protocol) as inst:
             assert inst.wavelength == 1500
 
 
 def test_power_getter():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
-        protocol += [("MEAS:POW?", 1e-3)]
+    for instr_cls, init_protocol in INSTR_CLS_PROTOCOL_LIST:
+        protocol = init_protocol + [("MEAS:POW?", 1e-3)]
         with expected_protocol(instr_cls, protocol) as inst:
             assert inst.power == 1e-3
 
 
 def test_power_density_getter():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
-        protocol += [("MEAS:PDEN?", 1e-3)]
+    for instr_cls, init_protocol in INSTR_CLS_PROTOCOL_LIST:
+        protocol = init_protocol + [("MEAS:PDEN?", 1e-3)]
         with expected_protocol(instr_cls, protocol) as inst:
             assert inst.power_density == 1e-3
 
 
 def test_energy_getter():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
-        protocol += [("MEAS:ENER?", 1e-3)]
+    for instr_cls, init_protocol in INSTR_CLS_PROTOCOL_LIST:
+        protocol = init_protocol + [("MEAS:ENER?", 1e-3)]
         with expected_protocol(instr_cls, protocol) as inst:
             inst.sensor_type = SensorTypes.PYROELECTRIC  # Override for test
             assert inst.energy == 1e-3
 
 
 def test_energy_raises():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
+    for instr_cls, protocol in INSTR_CLS_PROTOCOL_LIST:
         with expected_protocol(instr_cls, protocol) as inst:
             with pytest.raises(AttributeError):
                 assert inst.energy == 1e-3
 
 
 def test_energy_density_getter():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
-        protocol += [("MEAS:EDEN?", 1e-3)]
+    for instr_cls, init_protocol in INSTR_CLS_PROTOCOL_LIST:
+        protocol = init_protocol + [("MEAS:EDEN?", 1e-3)]
         with expected_protocol(instr_cls, protocol) as inst:
             inst.sensor_type = SensorTypes.PYROELECTRIC  # Override for test
             assert inst.energy_density == 1e-3
 
 
 def test_current_getter():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
-        protocol += [("MEAS:CURR?", 1e-3)]
+    for instr_cls, init_protocol in INSTR_CLS_PROTOCOL_LIST:
+        protocol = init_protocol + [("MEAS:CURR?", 1e-3)]
         with expected_protocol(instr_cls, protocol) as inst:
             assert inst.current == 1e-3
 
 
 def test_voltage_getter():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
-        protocol += [("MEAS:VOLT?", 1e-3)]
+    for instr_cls, init_protocol in INSTR_CLS_PROTOCOL_LIST:
+        protocol = init_protocol + [("MEAS:VOLT?", 1e-3)]
         with expected_protocol(instr_cls, protocol) as inst:
             inst.sensor_type = SensorTypes.PYROELECTRIC  # Override for test
             assert inst.voltage == 1e-3
 
 
 def test_temperature_getter():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
-        protocol += [("MEAS:TEMP?", 1e-3)]
+    for instr_cls, init_protocol in INSTR_CLS_PROTOCOL_LIST:
+        protocol = init_protocol + [("MEAS:TEMP?", 1e-3)]
         with expected_protocol(instr_cls, protocol) as inst:
             inst.is_temperature_sensor = True  # Override for test
             assert inst.temperature == 1e-3
 
 
 def test_frequency_getter():
-    for instr_cls, protocol in [
-        (ThorlabsPM100D, [PM100D_S130C_INIT_COMMS]),
-        (ThorlabsPM100D2, [PM100D2_S130C_INIT_COMMS]),
-    ]:
-        protocol += [("MEAS:FREQ?", 100)]
+    for instr_cls, init_protocol in INSTR_CLS_PROTOCOL_LIST:
+        protocol = init_protocol + [("MEAS:FREQ?", 100)]
         with expected_protocol(instr_cls, protocol) as inst:
             assert inst.frequency == 100
